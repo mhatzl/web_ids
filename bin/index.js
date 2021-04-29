4c01b80cae64d569563a8c7fc69c4f66823da914
Nadir Fejzic <nadirfejzo@gmail.com>
2021-04-29 17:20:38 +0200

    Trying to parse redis message

--- bin/index.js
+++ bin/index.js
@@ -3,11 +3,18 @@ const redis = require("redis");
 const subscriber = redis.createClient();
 
 let seconds = 0;
+let messageCount = 0;
 
 subscriber.on("message", function(channel, message) {
     messageCount += 1;
 
     console.log("Message from '" + channel + "': " + message);
+
+    const formatted = JSON.parse(message);
+
+    console.log("\n\n");
+    console.log("Parsed message: \n");
+    console.log(formatted);
 });
 
 subscriber.subscribe("nginx_logs_json");
